662e51e23e559924819bd493723f982a36a404e7
multipleof4 <planetrenox@protonmail.com>
2025-08-18 11:05:16 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
-import { defineConfig } from 'vite' 
+import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-export default defineConfig({ 
-  build: { outDir: 'docs' }, 
-  plugins: [ 
-    VitePWA({ 
-      registerType: 'autoUpdate', 
-      manifest: { 
-        name: 'Sune', 
-        short_name: 'Sune', 
-        start_url: 'https://sune.planetrenox.com/', 
-        display: 'standalone', 
-        icons: [ 
+export default defineConfig({
+  build: { outDir: 'docs' },
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Sune',
+        short_name: 'Sune',
+        start_url: 'https://sune.planetrenox.com/',
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#000000',
+        icons: [
           { src: 'https://sune.planetrenox.com/✺.png', sizes: '1024x1024', type: 'image/png' }
-        ] 
-      } 
-    }) 
-  ] 
+        ]
+      }
+    })
+  ]
 })
-
