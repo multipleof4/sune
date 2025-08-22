cf79b42c0d19fd2681104f31924dbf0d63cb7650
multipleof4 <planetrenox@protonmail.com>
2025-08-21 19:52:14 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,8 +14,8 @@ export default defineConfig({
         start_url: 'https://sune.planetrenox.com/',
         display: 'standalone',
         orientation: 'portrait',
-        theme_color: 'white',
-        background_color: 'black',
+        theme_color: '#FFFFFF',
+        background_color: '#000000',
         categories: ['productivity', 'utilities'],
         icons: [
           { src: 'https://sune.planetrenox.com/appstore_content/✺.png', sizes: '1024x1024', type: 'image/png' }
