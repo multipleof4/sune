ae324d389e20a9a527d15a4528e3dc5d544b97ed
multipleof4 <planetrenox@protonmail.com>
2025-08-26 11:01:35 -0700

    Delete functions/.well-known/assetlinks.json.js

--- functions/.well-known/assetlinks.json.js
+++
@@ -1,7 +0,0 @@
-export const onRequestGet = async () => {
-  const url = "https://raw.githubusercontent.com/multipleof4/sune/master/public/.well-known/assetlinks.json";
-  const r = await fetch(url, { cf: { cacheTtl: 0, cacheEverything: false } });
-  if (!r.ok) return new Response("not found", { status: 404 });
-  const body = await r.text();
-  return new Response(body, { status: 200, headers: { "content-type": "application/json; charset=utf-8", "cache-control": "no-store" } });
-};
