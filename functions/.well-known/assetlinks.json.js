70b5bb8fa4fa0e27f8c43d4a4dfe39016102d874
multipleof4 <planetrenox@protonmail.com>
2025-08-26 10:54:14 -0700

    Update assetlinks.json.js

--- functions/.well-known/assetlinks.json.js
+++ functions/.well-known/assetlinks.json.js
@@ -1,5 +1,5 @@
 export const onRequestGet = async () => {
-  const url = "https://raw.githubusercontent.com/OWNER/REPO/BRANCH/.well-known/assetlinks.json";
+  const url = "https://raw.githubusercontent.com/multipleof4/sune/master/public/.well-known/assetlinks.json";
   const r = await fetch(url, { cf: { cacheTtl: 0, cacheEverything: false } });
   if (!r.ok) return new Response("not found", { status: 404 });
   const body = await r.text();
