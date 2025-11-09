cbcd225367a265b42410a135b6ec29a5408a002c
multipleof4 <planetrenox@protonmail.com>
2025-11-08 19:45:53 -0800

    Delete src/core/state.js

--- src/core/state.js
+++
@@ -1,3 +0,0 @@
-import{el}from'../lib/dom.js';
-export const state=window.state={messages:[],busy:false,controller:null,currentThreadId:null,abortRequested:false,attachments:[],stream:{rid:null,bubble:null,meta:null,text:'',done:false}}
-export function clearChat(){el.suneHtml.dispatchEvent(new CustomEvent('sune:unmount'));state.messages=[];el.messages.innerHTML='';state.attachments=[];el.fileInput.value=''}
