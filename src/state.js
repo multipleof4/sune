6ca9ad3a544149513ea927b7fb25c478ba209590
multipleof4 <planetrenox@protonmail.com>
2025-11-18 21:04:55 -0800

    Delete src/state.js

--- src/state.js
+++
@@ -1,2 +0,0 @@
-export const state={messages:[],busy:false,controller:null,currentThreadId:null,abortRequested:false,attachments:[],stream:{rid:null,bubble:null,meta:null,text:'',done:false}}
-export const cacheStore=localforage.createInstance({name:'threads_cache',storeName:'streams_status'})
